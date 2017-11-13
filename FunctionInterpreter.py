57372f32e5cf04386f325f7c33afa969c6ed08f4
davidwilson826 <davidwilson@hanovernorwichschools.org>
2017-11-13 17:07:44 -0500

    Updated from Brython Server: 11/13/2017 5:07:43 PM

--- FunctionInterpreter.py
+++ FunctionInterpreter.py
@@ -1,25 +1,26 @@
 f_string = "3*x^2+12*x+11"#input("Please enter function ")
-#length = range(len(f_string))
 
-print(len(f_string))
-
-#f_list = [f_string[i] for i in length]
-
-n_test = ['0','1','2','3','4','5','6','7','8','9','10','.']
-operations = ['+','-','*','/']
-
-f_list = []
-i = 0
-
-while i < len(f_string):
-    if f_string[i] in n_test:
-        num = ''
-        while i < len(f_string) and f_string[i] in n_test:
-            num += f_string[i]
-            i += 1
-        f_list.append(float(num))
-    else:
-        f_list.append(f_string[i])
-        i += 1
+def f(f_string):
+    n_test = ['0','1','2','3','4','5','6','7','8','9','10','.']
+    operations = ['+','-','*','/']
     
-print(f_list)
+    f_list = []
+    i = 0
+    
+    while i < len(f_string):
+        if f_string[i] in n_test:
+            num = ''
+            while i < len(f_string) and f_string[i] in n_test:
+                num += f_string[i]
+                i += 1
+            f_list.append(float(num))
+        else:
+            f_list.append(f_string[i])
+            i += 1
+    
+    i = 0
+    while i < len(f_list)
+    
+    print(f_list)
+    
+f(f_string)
