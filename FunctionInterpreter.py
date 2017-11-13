31b92a93386f2a72646ba3b83e964d9f13dd7d41
davidwilson826 <davidwilson@hanovernorwichschools.org>
2017-11-13 17:11:52 -0500

    Updated from Brython Server: 11/13/2017 5:11:51 PM

--- FunctionInterpreter.py
+++ FunctionInterpreter.py
@@ -1,31 +1,33 @@
 f_string = "3*x^2+12*x+11"#input("Please enter function ")
 
-def f(f_string,x):
-    n_test = ['0','1','2','3','4','5','6','7','8','9','10','.']
-    operations = ['+','-','*','/']
-    
-    f_list = []
-    i = 0
-    
-    while i < len(f_string):
-        if f_string[i] in n_test:
-            num = ''
-            while i < len(f_string) and f_string[i] in n_test:
-                num += f_string[i]
-                i += 1
-            f_list.append(float(num))
-        else:
-            f_list.append(f_string[i])
+
+n_test = ['0','1','2','3','4','5','6','7','8','9','10','.']
+operations = ['+','-','*','/']
+
+f_list = []
+i = 0
+
+while i < len(f_string):
+    if f_string[i] in n_test:
+        num = ''
+        while i < len(f_string) and f_string[i] in n_test:
+            num += f_string[i]
             i += 1
-    
-    print(f_list)
-    
+        f_list.append(float(num))
+    else:
+        f_list.append(f_string[i])
+        i += 1
+
+print(f_list)
+
+def f(x):    
     i = 0
     while i < len(f_list):
         if f_list[i] == 'x':
             f_list[i] = x
+        i += 1
             
     print(f_list)
     
     
-f(f_string,1)
+f(1)
