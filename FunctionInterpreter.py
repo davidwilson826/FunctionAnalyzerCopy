b0623c30a0a625e9b6236e5da49856807346cd66
davidwilson826 <davidwilson@hanovernorwichschools.org>
2017-11-13 16:52:44 -0500

    Updated from Brython Server: 11/13/2017 4:52:42 PM

--- FunctionInterpreter.py
+++ FunctionInterpreter.py
@@ -3,9 +3,20 @@ length = range(len(f_string))
 
 f_list = [f_string[i] for i in length]
 
-digits = ['0','1','2','3','4','5','6','7','8','9','10']
+n_test = ['0','1','2','3','4','5','6','7','8','9','10','.']
 operations = ['+','-','*','/']
 
-for i in length:
-    if f_list[i]
+i = 0
+
+while i < length:
+    if f_string[i] in n_test:
+        num = ''
+        while f_string[i] in n_test:
+            num.append(f_string[i])
+            i += 1
+        f_list.append(float(num))
+    else:
+        f_list.append(f_string[i])
+    
+    num.append(f_string[i])
 print(f_list)
