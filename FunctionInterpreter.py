91296bfeae14b99e49e808e5d3dde1b598a120cc
davidwilson826 <davidwilson@hanovernorwichschools.org>
2017-11-13 12:03:30 -0500

    Updated from Brython Server: 11/13/2017 12:03:30 PM

--- FunctionInterpreter.py
+++ FunctionInterpreter.py
@@ -1,4 +1,4 @@
-f_string = input("Please enter function")
+f_string = "3*x^2+2*x"#input("Please enter function ")
 
-f_list = f_string.split()
+f_list = [f_string[i] for i in range(len(f_string))]
 print(f_list)
