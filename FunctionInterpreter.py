9a8ccd12cb0be7613b4938f1e13922825a22ab13
davidwilson826 <davidwilson@hanovernorwichschools.org>
2017-11-13 12:10:31 -0500

    Updated from Brython Server: 11/13/2017 12:10:26 PM

--- FunctionInterpreter.py
+++ FunctionInterpreter.py
@@ -1,4 +1,11 @@
-f_string = "3*x^2+2*x"#input("Please enter function ")
+f_string = "3*x^2+12*x"#input("Please enter function ")
+length = range(len(f_string))
 
-f_list = [f_string[i] for i in range(len(f_string))]
+f_list = [f_string[i] for i in length]
+
+digits = ['0','1','2','3','4','5','6','7','8','9','10']
+operations = ['+','-','*','/']
+
+for i in length:
+    if f_list[i]
 print(f_list)
